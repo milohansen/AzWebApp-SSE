538cd1d9551b0f445f137f047a40e260cd54cedf
Milo Hansen <milo.hansen@avanade.com>
2022-08-10 13:24:56 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AzWebApp-SSE
 An ultra-simple example repo for replicating SSE issues when Azure Web App middleware (CORS, Auth) are enabled.
+
+### Running Locally
+
+```node server.js```
+
+<!-- TODO: finish writing this -->
---
+++ server.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const fs = require("fs");
+const { createServer } = require("http");
+const crypto = require("crypto");
+
+const PORT = process.env.PORT || 4000;
+
+function makeId() {
+  return crypto.randomBytes(16).toString("hex");
+}
+
+async function sendGreetings(client) {
+  const his = ["Hi", "Bonjour", "Hola", "Ciao", "Zdravo"];
+  let i = 0;
+  while (!client.res.destroyed) {
+    client.res.write(`id: ${makeId()}\ndata: ${his[i]}\n\n`);
+    console.log("sending", his[i]);
+    i = (i + 1) % 5;
+    await new Promise(resolve => setTimeout(resolve, 5_000));
+  }
+}
+
+function sseHandler(req, res) {
+  const clientId = makeId();
+
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "x-client-id": clientId
+  };
+  res.writeHead(200, headers);
+
+  const newClient = {
+    id: clientId,
+    req,
+    res
+  };
+
+  req.on("close", () => {
+    console.log(`${clientId} Connection closed`);
+  });
+
+  sendGreetings(newClient);
+}
+
+console.log("creating server");
+const indexFile = fs.readFileSync(path.join(__dirname, "/front.html"));
+const configJsFile = fs.readFileSync(path.join(__dirname, "/config.js"));
+
+const server = createServer((req, res) => {
+  console.log("req", req.url);
+  if (req.url === "/config.js") {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(configJsFile);
+    return;
+  }
+  if (req.url === "/sse") {
+    sseHandler(req, res);
+  } else if (req.url === "/" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ error: "Resource not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`✴️  server ready at port ${PORT}`);
+});
